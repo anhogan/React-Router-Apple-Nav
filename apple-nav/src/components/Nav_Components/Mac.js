27acdc72b2ab51bc3a1ff74db30afbb5e0cfdc36
Amanda Hogan <ahogan@teamworks.com>
2020-01-28 12:14:14 -0500

    Add in Mac links

--- apple-nav/src/components/Nav_Components/Mac.js
+++ apple-nav/src/components/Nav_Components/Mac.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Nav_Components.css';
 
 const Mac = () => {
   return (
-    <div>Mac Page</div>
+    <div>
+      <div className="component_links">
+        <NavLink to="/">MacBook Air</NavLink>
+        <NavLink to="/">MacBook Pro 13"</NavLink>
+        <NavLink to="/">MacBook Pro 16"</NavLink>
+        <NavLink to="/">iMac</NavLink>
+        <NavLink to="/">iMac Pro</NavLink>
+        <NavLink to="/">Mac Pro</NavLink>
+        <NavLink to="/">Mac mini</NavLink>
+        <NavLink to="/">Compare</NavLink>
+        <NavLink to="/">Pro Display XDR</NavLink>
+        <NavLink to="/">Accessories</NavLink>
+        <NavLink to="/">Catalina</NavLink>
+      </div>
+    </div>
   )
 };
 
