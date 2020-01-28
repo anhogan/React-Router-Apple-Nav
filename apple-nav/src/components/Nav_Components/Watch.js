9cc03211dfeea0e136fa8c61131c6016e29583c5
Amanda Hogan <ahogan@teamworks.com>
2020-01-28 13:55:32 -0500

    iPad and Watch images

--- apple-nav/src/components/Nav_Components/Watch.js
+++ apple-nav/src/components/Nav_Components/Watch.js
@@ -1,21 +1,84 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Nav_Components.css';
+import watch from '../../apple_images/watch.svg';
+import watchNike from '../../apple_images/watch_nike.svg';
+import watchHermes from '../../apple_images/watch_hermes.svg';
+import watchCompare from '../../apple_images/watch_compare.svg';
+import bands from '../../apple_images/bands.svg';
+import airpods from '../../apple_images/airpods.svg';
+import watchAccessories from '../../apple_images/watch_accessories.svg';
+import watchOS from '../../apple_images/watchOS.svg';
 
 const Watch = () => {
   return (
     <div>
       <div className="component_links">
-        <NavLink to="/">Apple Watch Series 5</NavLink>
-        <NavLink to="/">Apple Watch Nike</NavLink>
-        <NavLink to="/">Apple Watch Hermes</NavLink>
-        <NavLink to="/">Apple Watch Edition</NavLink>
-        <NavLink to="/">iApple Watch Series 3</NavLink>
-        <NavLink to="/">Compare</NavLink>
-        <NavLink to="/">Bands</NavLink>
-        <NavLink to="/">AirPods</NavLink>
-        <NavLink to="/">Accessories</NavLink>
-        <NavLink to="/">watchOS</NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={watch} alt="Apple Watch Series 5" />
+            <span>Apple Watch Series 5</span>
+            <span className="newItem">New</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={watchNike} alt="Apple Watch Nike" />
+            <span>Apple Watch Nike</span>
+            <span className="newItem">New</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={watchHermes} alt="Apple Watch Hermes" />
+            <span>Apple Watch Hermes</span>
+            <span className="newItem">New</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={watch} alt="Apple Watch Edition" />
+            <span>Apple Watch Edition</span>
+            <span className="newItem">New</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={watch} alt="Apple Watch Series 3" />
+            <span>iApple Watch Series 3</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={watchCompare} alt="Compare" />
+            <span>Compare</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={bands} alt="Bands" />
+            <span>Bands</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={airpods} alt="AirPods" />
+            <span>AirPods</span>
+            <span className="newItem">New</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={watchAccessories} alt="Accessories" />
+            <span>Accessories</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={watchOS} alt="Watch OS" />
+            <span>watchOS</span>
+          </div>
+        </NavLink>
       </div>
     </div>
   )
--- apple-nav/src/components/Nav_Components/iPhone.js
+++ apple-nav/src/components/Nav_Components/iPhone.js
@@ -1,20 +1,76 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Nav_Components.css';
+import iPhone11 from '../../apple_images/iphone11.svg';
+import iPhone11Pro from '../../apple_images/iphone11_pro.svg';
+import iPhone8 from '../../apple_images/iphone8.svg';
+import iPhone_compare from '../../apple_images/iphone_compare.svg';
+import apple_card from '../../apple_images/apple_card.svg';
+import airpods from '../../apple_images/airpods_white.svg';
+import iPhone_accessories from '../../apple_images/iphone_accessories.svg';
+import iOS from '../../apple_images/ios.svg';
 
 const iPhone = () => {
   return (
     <div>
       <div className="component_links_dark">
-        <NavLink to="/">iPhone 11 Pro</NavLink>
-        <NavLink to="/">iPhone 11</NavLink>
-        <NavLink to="/">iPhone XR</NavLink>
-        <NavLink to="/">iPhone 8</NavLink>
-        <NavLink to="/">Compare</NavLink>
-        <NavLink to="/">Apple Card</NavLink>
-        <NavLink to="/">AirPods</NavLink>
-        <NavLink to="/">Accessories</NavLink>
-        <NavLink to="/">iOS 13</NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={iPhone11Pro} alt="iPhone 11 Pro" />
+            <span>iPhone 11 Pro</span>
+            <span className="newItem">New</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={iPhone11} alt="iPhone 11" />
+            <span>iPhone 11</span>
+            <span className="newItem">New</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={iPhone11} alt="iPhone XR" />
+            <span>iPhone XR</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={iPhone8} alt="iPhone 8" />
+            <span>iPhone 8</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={iPhone_compare} alt="Compare" />
+            <span>Compare</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={apple_card} alt="Apple Card" />
+            <span>Apple Card</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={airpods} alt="Airpods" />
+            <span>AirPods</span>
+            <span className="newItem">New</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={iPhone_accessories} alt="Accessories" />
+            <span>Accessories</span>
+          </div>
+        </NavLink>
+        <NavLink to="/">
+          <div className="individual_link_container">
+            <img src={iOS} alt="iOS 13" />
+            <span>iOS 13</span>
+          </div>
+        </NavLink>
       </div>
     </div>
   )
