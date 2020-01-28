52b4d4d1707c5762eb5740e61256a8b43d34181c
Amanda Hogan <ahogan@teamworks.com>
2020-01-28 10:54:28 -0500

    Add in NavLinks to each page

--- apple-nav/src/components/NavWrapper.js
+++ apple-nav/src/components/NavWrapper.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavWrapper = () => {
   return (
-    <div></div>
+    <NavLink to="/"><img src="" alt="Apple Logo" /></NavLink>
+    <NavLink to="/mac/">Mac</NavLink>
+    <NavLink to="/ipad/">iPad</NavLink>
+    <NavLink to="/iphone/">iPhone</NavLink>
+    <NavLink to="/watch/">Watch</NavLink>
+    <NavLink to="/tv/">TV</NavLink>
+    <NavLink to="/music/">Music</NavLink>
+    <NavLink to="support.apple.com">Support</NavLink>
+    <img src="" alt="Search" />
+    <img src="" alt="Shopping Bag" />
   )
 };
 
