0b8edd067847bffb55b9f8d44593763ffd91b671
Amanda Hogan <ahogan@teamworks.com>
2020-01-28 13:30:01 -0500

    Render correct images for each item

--- apple-nav/src/components/Nav_Components/Mac.js
+++ apple-nav/src/components/Nav_Components/Mac.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Nav_Components.css';
 import macbook from '../../apple_images/macbook.svg';
+import imac from '../../apple_images/imac.svg';
+import mac_pro from '../../apple_images/mac_pro.svg';
+import macmini from '../../apple_images/macmini.svg';
+import compare from '../../apple_images/mac_compare.svg';
+import proDisplay from '../../apple_images/pro_display.svg';
+import accessories from '../../apple_images/headphones.svg';
+import macOS from '../../apple_images/macOS.svg';
 
 // Add in Image
 // Add in New text
@@ -33,51 +40,51 @@ const Mac = () => {
         </NavLink>
         <NavLink to="/">
           <div className="individual_link_container">
-            <img src={macbook} alt="iMac" />
+            <img src={imac} alt="iMac" />
             <span>iMac</span>
           </div>
         </NavLink>
         <NavLink to="/">
           <div className="individual_link_container">
-            <img src={macbook} alt="iMac Pro" />
+            <img src={imac} alt="iMac Pro" />
             <span>iMac Pro</span>
           </div>
         </NavLink>
         <NavLink to="/">
           <div className="individual_link_container">
-            <img src={macbook} alt="Mac Pro" />
+            <img src={mac_pro} alt="Mac Pro" />
             <span>Mac Pro</span>
             <span className="newItem">New</span>
           </div>
         </NavLink>
         <NavLink to="/">
           <div className="individual_link_container">
-            <img src={macbook} alt="Mac Mini" />
+            <img src={macmini} alt="Mac Mini" />
             <span>Mac mini</span>
           </div>
         </NavLink>
         <NavLink to="/">
           <div className="individual_link_container">
-            <img src={macbook} alt="Compare" />
+            <img src={compare} alt="Compare" />
             <span>Compare</span>
           </div>
         </NavLink>
         <NavLink to="/">
           <div className="individual_link_container">
-            <img src={macbook} alt="Pro Display XDR" />
+            <img src={proDisplay} alt="Pro Display XDR" />
             <span>Pro Display XDR</span>
             <span className="newItem">New</span>
           </div>
         </NavLink>
         <NavLink to="/">
           <div className="individual_link_container">
-            <img src={macbook} alt="Accessories" />
+            <img src={accessories} alt="Accessories" />
             <span>Accessories</span>
           </div>
         </NavLink>
         <NavLink to="/">
           <div className="individual_link_container">
-            <img src={macbook} alt="Catalina" />
+            <img src={macOS} alt="Catalina" />
             <span>Catalina</span>
           </div>
         </NavLink>
