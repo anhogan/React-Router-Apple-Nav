f6d5b1b51defcd23cdb9c7139251ce3f92c029e8
Amanda Hogan <ahogan@teamworks.com>
2020-01-28 11:48:49 -0500

    Nav bar styling

--- apple-nav/src/components/NavWrapper.js
+++ apple-nav/src/components/NavWrapper.js
@@ -4,28 +4,34 @@ import { Mac, iPad, iPhone, Watch, TV, Music, Support } from './Nav_Components';
 import apple_logo from '../apple_logo.png';
 import search from '../search.png';
 import shopping_bag from '../shopping_bag.png';
+import './NavWrapper.css';
 
 const NavWrapper = () => {
   return (
     <div>
-      <NavLink to="/"><img src={apple_logo} alt="Apple Logo" /></NavLink>
-      <NavLink to="/mac">Mac</NavLink>
-      <NavLink to="/ipad">iPad</NavLink>
-      <NavLink to="/iphone">iPhone</NavLink>
-      <NavLink to="/watch">Watch</NavLink>
-      <NavLink to="/tv">TV</NavLink>
-      <NavLink to="/music">Music</NavLink>
-      <NavLink to="/support">Support</NavLink>
-      <img src={search} alt="Search" />
-      <img src={shopping_bag} alt="Shopping Bag" />
-
-      <Route path="/mac" component={Mac} />
-      <Route path="/ipad" component={iPad} />
-      <Route path="/iphone" component={iPhone} />
-      <Route path="/watch" component={Watch} />
-      <Route path="/tv" component={TV} />
-      <Route path="/music" component={Music} />
-      <Route path="/support" component={Support} />
+      <div className="nav_wrapper">
+        <div className="nav_links">
+          <NavLink to="/"><img src={apple_logo} alt="Apple Logo" /></NavLink>
+          <NavLink to="/mac">Mac</NavLink>
+          <NavLink to="/ipad">iPad</NavLink>
+          <NavLink to="/iphone">iPhone</NavLink>
+          <NavLink to="/watch">Watch</NavLink>
+          <NavLink to="/tv">TV</NavLink>
+          <NavLink to="/music">Music</NavLink>
+          <NavLink to="/support">Support</NavLink>
+        </div>
+        <img className="nav_image" src={search} alt="Search" />
+        <img className="nav_image" src={shopping_bag} alt="Shopping Bag" />
+      </div>
+      <div>
+        <Route path="/mac" component={Mac} />
+        <Route path="/ipad" component={iPad} />
+        <Route path="/iphone" component={iPhone} />
+        <Route path="/watch" component={Watch} />
+        <Route path="/tv" component={TV} />
+        <Route path="/music" component={Music} />
+        <Route path="/support" component={Support} />
+      </div>
     </div>
   )
 };
